a6439f3dc3e3e344096d7ebe4c43cb0d56159fa4
Rayrisson Vinicius Alves de Lima <rayrissonvinicius@gmail.com>
2022-09-25 14:36:30 -0300

    Update styles

--- web/src/components/GameBanner/index.tsx
+++ web/src/components/GameBanner/index.tsx
@@ -8,14 +8,18 @@ const GameBanner = ({ bannerUrl, title, adsCount }: GameBannerProps) => {
   return (
     <a
       href=""
-      className="relative rounded-lg overflow-hidden"
+      className="relative rounded-lg overflow-hidden w-[204px]"
       onClick={(e) => e.preventDefault()}
     >
-      <img src={bannerUrl} />
+      <img src={bannerUrl} className="w-full h-full rounded-[0.625rem]" />
 
-      <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0">
-        <strong className="font-bold text-white block">{title}</strong>
-        <span className="text-zinc-300 text-sm block">{adsCount} anúncios</span>
+      <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 rounded-lg">
+        <strong className="font-bold text-white block text-ellipsis whitespace-nowrap overflow-hidden">
+          {title}
+        </strong>
+        <span className="text-zinc-300 text-sm block text-ellipsis whitespace-nowrap overflow-hidden">
+          {adsCount} anúncios
+        </span>
       </div>
     </a>
   );
