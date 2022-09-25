0d352adc95cd17bc7f5b731edd90149211b9189a
Rayrisson Vinicius Alves de Lima <rayrissonvinicius@gmail.com>
2022-09-25 14:37:26 -0300

    Add red border with error to input

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -1,10 +1,14 @@
-import { InputHTMLAttributes } from "react";
+interface InputProps extends React.HTMLProps<HTMLInputElement> {
+  error?: boolean;
+}
 
-const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
+const Input = ({ error = false, ...props }: InputProps) => {
   return (
     <input
       {...props}
-      className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500"
+      className={`bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 autofill:bg-zinc-500 ${
+        error && "border border-red-500 focus:border-red-500 outline-none"
+      }`}
     />
   );
 };
